ca86dcef26a24d71bea8ae9e32c3ddf208b1314b
jatin14493 <mahajanjatin14@gmail.com>
2020-05-25 17:34:20 +0530

    Code Refactor

--- Application.java
+++ Application.java
@@ -9,11 +9,11 @@ import java.util.stream.Collectors;
 
 public class Application {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         RuleEvaluator ruleEvaluator = new RuleEvaluator();
         List<List<Rule>> oldRules = new ArrayList<>();
 
-        Rule ruleOld1 = new Rule("1", "Amount", "GT 100");
+        Rule ruleOld1 = new Rule("1", "Amount", "GT 100 && LTE 200");
         Rule ruleOld2 = new Rule("2", "Customer", "CONTAINS 1,2");
         Rule ruleOld3 = new Rule("3", "Department", "CONTAINS 1");
         List<Rule> rulesOld = new ArrayList<>();
@@ -21,9 +21,9 @@ public class Application {
         rulesOld.add(ruleOld2);
         rulesOld.add(ruleOld3);
 
-        Rule ruleOld11 = new Rule("1", "Amount", "GT 100");
-        Rule ruleOld12 = new Rule("2", "Customer", "CONTAINS 1,2");
-        Rule ruleOld13 = new Rule("3", "Department", "CONTAINS 1,2");
+        Rule ruleOld11 = new Rule("1", "Amount", "GT 200 && LT 1000");
+        Rule ruleOld12 = new Rule("2", "Customer", "CONTAINS 3,4");
+        Rule ruleOld13 = new Rule("3", "Department", "CONTAINS 2");
         List<Rule> rulesOld1 = new ArrayList<>();
         rulesOld1.add(ruleOld11);
         rulesOld1.add(ruleOld12);
@@ -32,9 +32,9 @@ public class Application {
         oldRules.add(rulesOld);
         oldRules.add(rulesOld1);
 
-        Rule ruleNew1 = new Rule("1", "Amount", "LT 100");
-        Rule ruleNew2 = new Rule("2", "Customer", "NOT_CONTAINS 1,2");
-        Rule ruleNew3 = new Rule("3", "Department", "NOT_CONTAINS 1");
+        Rule ruleNew1 = new Rule("1", "Amount", "GTE 1000");
+        Rule ruleNew2 = new Rule("2", "Customer", "NOT_CONTAINS 1,2,3,4");
+        Rule ruleNew3 = new Rule("3", "Department", "NOT_CONTAINS 1,2");
         List<Rule> rulesNew = new ArrayList<>();
         rulesNew.add(ruleNew1);
         rulesNew.add(ruleNew2);
